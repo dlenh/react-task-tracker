535807984b3b26422174559751eaed9424e00590
David Lenh <dlenh425@gmail.com>
2022-09-01 10:16:12 -0700

    Used useState form react for tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,23 +1,24 @@
-const tasks = [
-    {
-        id: 1,
-        text: "Doctors Appointment",
-        day: "Feb 5th at 2:30pm",
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: "Meeting at School",
-        day: "Feb 6th at 1:30pm",
-        reminder: true
-    }
-]
+import { useState } from "react"
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+            {
+                id: 1,
+                text: "Doctors Appointment",
+                day: "Feb 5th at 2:30pm",
+                reminder: true,
+            },
+            {
+                id: 2,
+                text: "Meeting at School",
+                day: "Feb 6th at 1:30pm",
+                reminder: true
+            }
+        ])
   return (
     <>
         {tasks.map((task) => (
-        <h3>{task.text}</h3>
+        <h3 key={task.id}>{task.text}</h3>
         ))}
     </>
   )
