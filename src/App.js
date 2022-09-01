81e26c993b9b6802d63372b04f80da229c8a23cd
David Lenh <dlenh425@gmail.com>
2022-09-01 10:32:47 -0700

    Added a third task into the array

--- src/App.js
+++ src/App.js
@@ -15,6 +15,12 @@ function App() {
         text: "Meeting at School",
         day: "Feb 6th at 1:30pm",
         reminder: true
+    },
+    {
+        id: 3,
+        text: "Food Shopping",
+        day: "Feb 5th at 2:30pm",
+        reminder: false,
     }
 ])
   return (
